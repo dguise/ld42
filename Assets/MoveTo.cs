b0ddbd08907e3eac7f04fcc4443709f895de5bf3
theyuppi <perjonaserik@gmail.com>
2018-08-11 09:41:11 +0200

    Space eater made new friend. SpaceInvader. Space to get more space!

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(4.310709f, 1.85f, 3.417806f), Time.deltaTime);
+	
+		
+	}
+}
