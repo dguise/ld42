50d8a55f832a87f0d2cf88d5f62df9acb53deb00
Maakep <schon.fredrik@gmail.com>
2018-08-11 22:34:57 +0200

    Add death screen & fader

---
+++ Assets/Scripts/DeathScreen/DeathScreenManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreenManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Fader.Instance.FadeIn();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Gui/Fader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    private static Fader _instance = null;
+    public static Fader Instance {
+        get {
+            if (_instance == null)
+                _instance = Instantiate(Resources.Load<GameObject>("FaderCanvas")).GetComponentInChildren<Fader>();
+            return _instance;
+        }
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    Image fadeImage;
+
+    private Color invisible = new Color(0, 0, 0, 0);
+    private Color visible = new Color(0, 0, 0, 1);
+
+    private float speed = 1;
+
+    private Coroutine fadeRoutine = null;
+
+    void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        fadeImage.color = invisible;
+    }
+
+    public void FadeOut(Action doneCallback = null)
+    {
+        fadeImage = GetComponent<Image>();
+        if (fadeRoutine == null)
+            fadeRoutine = StartCoroutine(_FadeOut(doneCallback));
+    }
+
+    private IEnumerator _FadeOut(Action doneCallback)
+    {
+        float tick = 0f;
+        do
+        {
+            fadeImage.color = Color.Lerp(invisible, visible, tick * Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        } while (fadeImage.color != visible);
+
+        if (doneCallback != null)
+            doneCallback();
+    }
+
+    public void FadeIn(Action doneCallback = null)
+    {
+        fadeImage = GetComponent<Image>();
+        if (fadeRoutine == null)
+            fadeRoutine = StartCoroutine(_FadeIn(doneCallback));
+    }
+
+    private IEnumerator _FadeIn(Action doneCallback)
+    {
+        float tick = 0f;
+        do
+        {
+            fadeImage.color = Color.Lerp(visible, invisible, tick * Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        } while (fadeImage.color != invisible);
+
+        if (doneCallback != null)
+            doneCallback();
+    }
+}
