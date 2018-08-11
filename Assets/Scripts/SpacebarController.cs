0836bb06a33a95b9ed23806b9cf99da23c17ba16
Per <perjonaserik@gmail.com>
2018-08-11 18:13:22 +0200

    Push med fixad ps och disc för player

--- Assets/Scripts/SpacebarController.cs
+++ Assets/Scripts/SpacebarController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpacebarController : MonoBehaviour {
+public class SpacebarController : MonoBehaviour
+{
     private Rigidbody2D rb;
 
     private float spaceStartTime;
@@ -15,35 +16,64 @@ public class SpacebarController : MonoBehaviour {
     private float discEndScale = 1.4f;
 
     private Transform engineDisc;
+    private ParticleSystem ps;
 
-	void Start () {
+    void Start()
+    {
         rb = this.GetComponent<Rigidbody2D>();
         engineDisc = transform.Find("EngineDisc");
-	}
+        ps = transform.Find("EngineParticles").GetComponent<ParticleSystem>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             spaceStartTime = Time.time;
-        } else if(Input.GetKeyUp(KeyCode.Space)) {
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
             float timeDiff = Time.time - spaceStartTime;
             rb.AddForce(-transform.up * timeDiff * timeScale);
-        } else if(Input.GetKey(KeyCode.Space)) {
+            DisableStuff();
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            if (!engineDisc.gameObject.activeSelf)
+                EnableStuff();
             ChargeUp();
         }
 
-        if (Input.GetKey(KeyCode.A)) {
-			float t = turnSpeed * Time.deltaTime;
-			transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + Mathf.LerpAngle(0, 5, t));
-        } else if (Input.GetKey(KeyCode.D)) {
-			float t = turnSpeed * Time.deltaTime;
-			transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + Mathf.LerpAngle(0, -5, t));
+        if (Input.GetKey(KeyCode.A))
+        {
+            float t = turnSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + Mathf.LerpAngle(0, 5, t));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            float t = turnSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + Mathf.LerpAngle(0, -5, t));
         }
     }
 
-    void ChargeUp() {
+    void ChargeUp()
+    {
         float timeFactor = Mathf.Min((Time.time - spaceStartTime) / spaceMaxTime, 1f);
         float scale = discStartScale + (discEndScale - discStartScale) * timeFactor;
         engineDisc.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    void DisableStuff()
+    {
+        engineDisc.gameObject.SetActive(false);
+        ps.gameObject.SetActive(false);
+        ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    void EnableStuff()
+    {
+        engineDisc.gameObject.SetActive(true);
+        ps.gameObject.SetActive(true);
+        ps.Play();
     }
 }
