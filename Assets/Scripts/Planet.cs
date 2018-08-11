2afad6c5ff8c5ee46a76122d2cf92b0311ec66bc
Per <perjonaserik@gmail.com>
2018-08-11 17:52:16 +0200

    Oj, titta där borta! Där! Ser du det där ute?? WOW! Titta ut!

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -9,23 +9,22 @@ public class Planet : MonoBehaviour {
 
 	private Rigidbody2D rbPlayer;
 
-	// Use this for initialization
 	void Start () {
 		rbPlayer = player.GetComponent<Rigidbody2D>();
+        
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		float dist = Vector2.Distance(transform.position, player.transform.position);
+        
+        float dist = Vector2.Distance(transform.position, player.transform.position);
 		if (dist < gravitationRadius) {
 			Vector2 force = (transform.position - player.transform.position).normalized * gravitation * (gravitationRadius - dist);
-			//Debug.Log(force.magnitude);
 			rbPlayer.AddForce(force);
 		}
 	}
 
 	void OnDrawGizmos() {
-		Gizmos.color = Color.red;
+        Gizmos.color = Random.ColorHSV();
 		Gizmos.DrawWireSphere(transform.position, gravitationRadius);
 	}
 }
