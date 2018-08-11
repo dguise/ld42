aec241a3452bf898be60a64ba956b49bb96da427
laxen <alex.ganslandt@gmail.com>
2018-08-11 21:34:49 +0200

    Add super cool charge effects

--- Assets/Scripts/SpacebarController.cs
+++ Assets/Scripts/SpacebarController.cs
@@ -8,21 +8,35 @@ public class SpacebarController : MonoBehaviour
 
     private float spaceStartTime;
     private float spaceMaxTime = 2f;
+    private float chargePower;
 
-    public float timeScale;
+    public float chargePowerMax;
     public float turnSpeed;
+    public float chargeTrailTime;
+
+    private float chargeTrailTimestamp;
 
     private float discStartScale = 0.3f;
     private float discEndScale = 1.4f;
 
-    private Transform engineDisc;
-    private ParticleSystem ps;
+    private Transform engineDisc1;
+    private ParticleSystem ps1;
+    private Transform engineDisc2;
+    private ParticleSystem ps2;
+
+    private ParticleSystem ecp1;
+    private ParticleSystem ecp2;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        engineDisc = transform.Find("EngineDisc");
-        ps = transform.Find("EngineParticles").GetComponent<ParticleSystem>();
+        engineDisc1 = transform.Find("EngineDisc1");
+        ps1 = transform.Find("EngineParticles1").GetComponent<ParticleSystem>();
+        engineDisc2 = transform.Find("EngineDisc2");
+        ps2 = transform.Find("EngineParticles2").GetComponent<ParticleSystem>();
+
+        ecp1 = transform.Find("EngineChargeParticles1").GetComponent<ParticleSystem>();
+        ecp2 = transform.Find("EngineChargeParticles2").GetComponent<ParticleSystem>();
     }
 
     void Update()
@@ -33,13 +47,17 @@ public class SpacebarController : MonoBehaviour
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
-            float timeDiff = Time.time - spaceStartTime;
-            rb.AddForce(-transform.up * timeDiff * timeScale);
+            rb.AddForce(-transform.up * chargePower);
+            ecp1.gameObject.SetActive(true);
+            ecp2.gameObject.SetActive(true);
+            ecp1.Play();
+            ecp2.Play();
             DisableStuff();
+            chargeTrailTimestamp = Time.time;
         }
         else if (Input.GetKey(KeyCode.Space))
         {
-            if (!engineDisc.gameObject.activeSelf)
+            if (!engineDisc1.gameObject.activeSelf)
                 EnableStuff();
             ChargeUp();
         }
@@ -54,26 +72,39 @@ public class SpacebarController : MonoBehaviour
             float t = turnSpeed * Time.deltaTime;
             transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + Mathf.LerpAngle(0, -5, t));
         }
+
+        if (Time.time - chargeTrailTimestamp > chargeTrailTime) {
+            ecp1.Stop();
+            ecp2.Stop();
+        }
     }
 
     void ChargeUp()
     {
         float timeFactor = Mathf.Min((Time.time - spaceStartTime) / spaceMaxTime, 1f);
+        chargePower = chargePowerMax * timeFactor;
         float scale = discStartScale + (discEndScale - discStartScale) * timeFactor;
-        engineDisc.transform.localScale = new Vector3(scale, scale, scale);
+        engineDisc1.transform.localScale = new Vector3(scale, scale, scale);
+        engineDisc2.transform.localScale = new Vector3(scale, scale, scale);
     }
 
     void DisableStuff()
     {
-        engineDisc.gameObject.SetActive(false);
-        ps.gameObject.SetActive(false);
-        ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        engineDisc1.gameObject.SetActive(false);
+        engineDisc2.gameObject.SetActive(false);
+        ps1.gameObject.SetActive(false);
+        ps2.gameObject.SetActive(false);
+        ps1.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        ps2.Stop(true, ParticleSystemStopBehavior.StopEmitting);
     }
 
     void EnableStuff()
     {
-        engineDisc.gameObject.SetActive(true);
-        ps.gameObject.SetActive(true);
-        ps.Play();
+        engineDisc1.gameObject.SetActive(true);
+        engineDisc2.gameObject.SetActive(true);
+        ps1.gameObject.SetActive(true);
+        ps2.gameObject.SetActive(true);
+        ps1.Play();
+        ps2.Play();
     }
 }
