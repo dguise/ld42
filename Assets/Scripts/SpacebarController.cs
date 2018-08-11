d5f87fc03a8c59ace8f7ac160b9dba959cf39e5a
Per <perjonaserik@gmail.com>
2018-08-11 12:37:08 +0200

    2d

---
+++ Assets/Scripts/SpacebarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacebarController : MonoBehaviour {
+
+    RigidBody2D rb;
+	void Start () {
+        rb = this.GetComponent<RigidBody2D>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
