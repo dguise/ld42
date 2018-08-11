81845cf7dd575dda6248cf9185a16a93420b68c8
Per <perjonaserik@gmail.com>
2018-08-11 22:01:36 +0200

    SLACKER EMIL - roterande sol, rotationsscript, ny sprite för sol

---
+++ Assets/Scripts/RotateSunLikeAKebabSpett.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSunLikeAKebabSpett : MonoBehaviour {
+
+
+    public float rotationX = 0f;
+    public float rotationY = 0f;
+    public float rotationZ = 0.01f;
+
+    void Update () {
+        this.transform.Rotate(rotationX, rotationY, rotationZ);
+    }
+}
