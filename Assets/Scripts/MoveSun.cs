94a873557d2177807ad80a5f822a079ccaaf3198
laxen <alex.ganslandt@gmail.com>
2018-08-11 23:26:31 +0200

    actually fix sun though

---
+++ Assets/Scripts/MoveSun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSun : MonoBehaviour {
+	public float speed;
+	public Transform player;
+	public float rubberBandRadius;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = transform.position;
+		if (player.position.x - transform.position.x > rubberBandRadius) {
+			v.x = player.position.x - rubberBandRadius;
+		}
+		v.x += speed * Time.deltaTime;
+		transform.position = v;
+	}
+}
