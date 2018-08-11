28b9d801db7a707cc2c8daf6ff4b0d930b960732
Per <perjonaserik@gmail.com>
2018-08-11 12:29:47 +0200

    Sunnyside vs Spacebar in a battle of space
    i min scen

---
+++ Assets/Flames.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flames : MonoBehaviour
+{
+    float shakeIntensity = 0.1f;
+    Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+
+        var random = Random.Range(0.0f, 10.0f);
+        if (random > 3)
+        {
+            Vector3 newPos = pos + Random.insideUnitSphere * shakeIntensity;
+            newPos.z = pos.z;
+
+            transform.position = newPos;
+        }
+    }
+}
